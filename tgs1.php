05489afc0f1cd1237a873b103e8e2c140b50ba7d
tomyspt <saputratomy325@gmail.com>
2017-09-04 10:29:59 +0700

    mengumpulkan tugas 05

---
+++ tgs1.php
@@ -0,0 +1,6 @@
+<?php
+	echo "Berikut daftar banyaknya bilangan ganjil dari 1 sampai 100 : ";
+	for($i = 1; $i <= 100; $i = $i + 2){
+		echo "$i ";
+	}
+?>
---
+++ tgs2.php
@@ -0,0 +1,6 @@
+<?php
+	echo "Berikut tampilan bilangan kelipatan 7 yang kurang dari 100 : ";
+	for($i = 7; $i <= 100; $i = $i * 2){
+		echo "$i ";
+	}
+?>
