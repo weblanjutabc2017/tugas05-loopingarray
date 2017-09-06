afbb48572e93ace4f2d0bdfa5f2dad58894adf0c
SitiNuriaR <nuriarachma1106@gmail.com>
2017-09-06 21:13:16 +0700

    menambahkan file tugas 05 mengenai looping dan array nomor satu sampai dengan tiga.

---
+++ 05_AB_10510/Tugas05.php
@@ -0,0 +1,13 @@
+<? php
+	$ganjil = 0;
+
+	while ($ganjil <= 100){
+		if ($ganjil %2 != 0){
+			$ganjil++;
+		}
+		
+	}
+		echo "Bilangan ganjil yang ada dalam angka 1-100 sebanyak : ".$ganjil;
+		echo "<br />";
+
+?>
